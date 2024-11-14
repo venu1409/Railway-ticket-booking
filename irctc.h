3731f91afdc6365a1ff4fed322c365bf511f4ec1
venu1409 <ramagangadhar1212@gmail.com>
2024-11-14 13:03:13 +0530

    Create irctc.h

---
+++ irctc.h
@@ -0,0 +1,25 @@
+#include "irctc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void createTicketFile(int Train_Number, char from[], char to[], int day, int month, int year, int Seats, char names[][250], char Seat_Preference[], long long PNR) {
+    char filename[50];
+    sprintf(filename, "ticket_%lld.txt", PNR);
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error creating ticket file.\n");
+        return;
+    }
+
+    fprintf(file, "PNR: %lld\n", PNR);
+    fprintf(file, "Train Number: %d\n", Train_Number);
+    fprintf(file, "From: %s\n", from);
+    fprintf(file, "To: %s\n", to);
+    fprintf(file, "Date of Journey: %02d-%02d-%d\n", day, month, year);
+    fprintf(file, "Number of Seats: %d\n", Seats);
+    for (int i = 0; i < Seats; i++) {
+        fprintf(file, "Passenger %d: %s, Seat Preference: %c\n", i + 1, names[i], Seat_Preference[i]);
+    }
+    fclose(file);
+    printf("Ticket file created: %s\n", filename);
+}
